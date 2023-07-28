0cae17f03077eca314ddc9f02f0d5a9d1f0e6043
martin goñi <you@example.com>
2023-07-28 13:57:16 -0300

    segundo commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import './Button.css';
+
+const Button = ({icon, handleClick}) => {
+  return (
+    <div className='button__box'>
+    <button className="button" onClick={handleClick}>{icon}</button>
+    <div className='button__shadow'></div>
+    </div>
+  )
+}
+
+export {Button}
